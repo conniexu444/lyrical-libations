4ed16e7705669fece7f03fc6a566036959883462
conniexu444 <conniexu444@gmail.com>
2025-09-17 11:52:32 -0400

    Adding in files to make deployment work

--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -1,11 +1,9 @@
 export default function Shows() {
-    return (
-      <div>
-        <h1 className="text-3xl font-[var(--font-display)] mb-4 text-center">
-          Upcoming Shows
-        </h1>
-        
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1 className="text-3xl font-[var(--font-display)] mb-4 text-center">
+        Upcoming Shows
+      </h1>
+    </div>
+  );
+}
